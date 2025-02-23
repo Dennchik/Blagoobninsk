386c33f07fe1c76e11b5428e827807e05fb6770d
Alexandr <104072970+Dennchik@users.noreply.github.com>
2025-02-23 21:44:17 +0300

    23 Февраль  2024

--- #src/js/app.js
+++ #src/js/app.js
@@ -4,9 +4,13 @@ import {
 //* ----------------------------------------------------------------------------
 document.addEventListener('DOMContentLoaded', () => {
 	const isMobile = /Mobi|Android/i.test(navigator.userAgent);
-	cassieEvans();
+
 	if (!isMobile) {
+		cassieEvans();
 		tlVerticalOpacity();
+	} else {
+		const parallax = document.querySelector('.parallax');
+		parallax.style.opacity = '0.1';
 	}
 	setTimeout(() => {
 		tlRotateIcon();
--- #src/js/main copy.js
+++ #src/js/main copy.js
@@ -1,6 +1,10 @@
 import loaded from './assets/preloader.js';
 
 loaded('.preloader');
+import {
+	timeLineHeaderItem,
+	timeLineTextItem,
+} from './animations/anime-js.jsx';
 
 // import { dynamicAdaptive } from './assets/dynamic-adaptive.js';
 
@@ -8,49 +12,12 @@ loaded('.preloader');
 
 import returnToSavedPosition from './modules/return-position.js';
 returnToSavedPosition();
+
 //* ---------------- Плавная прокрутка страницы до позиции ---------------------
 import { anchorsSmoothScrolling } from './assets/anchors-smooth-scrolling.js';
 
 anchorsSmoothScrolling();
-
 //* ----------------------------------------------------------------------------
-const menuList = document.querySelector('.menu-list');
-const burgerButtons = document.querySelector('.burger-button');
-const listContent = document.querySelector('.main-content__menu-list');
-
-burgerButtons.addEventListener('click', () => {
-	const backgroundColorTransparent = getComputedStyle(
-		document.documentElement).getPropertyValue(
-			'--background-color-transparent');
-	console.log(menuList);
-
-	burgerButtons.firstChild.classList.toggle('is-active');
-	menuList.classList.toggle('_open-list');
-	document.body.classList.toggle('no-scroll');
-	if (menuList.classList.contains('_open-list')) {
-		// listContent.style.pointerEvents = 'all';
-		listContent.style.backgroundColor = backgroundColorTransparent;
-		document.body.classList.add('no-scroll');
-	} else {
-		document.body.classList.remove('no-scroll');
-		listContent.style.backgroundColor = 'transparent';
-		// listContent.style.pointerEvents = 'none';
-	}
-});
-
-// const closeButton = document.querySelector('.project-list__close-button');
-// closeButton.addEventListener('click', () => {
-// 	document.body.classList.remove('no-scroll');
-// 	burgerButtons.firstChild.classList.remove('is-active');
-// 	projectList.classList.remove('_open-list');
-// 	listContent.style.pointerEvents = 'none';
-// 	listContent.style.backgroundColor = 'transparent';
-// });
-//* ----------------------------------------------------------------------------
-import {
-	timeLineHeaderItem,
-	timeLineTextItem,
-} from './animations/anime-js.jsx';
 
 document.addEventListener('DOMContentLoaded', function () {
 	const textItem = document.querySelector('.performance__text');
@@ -58,11 +25,9 @@ document.addEventListener('DOMContentLoaded', function () {
 		timeLineTextItem();
 	}
 
-
 	//* ------------------------------- Date -------------------------------------
 	let dateContainer = document.querySelector('.performance__date');
 	if (!dateContainer) return;
-
 	let now = new Date();
 	let options = { month: 'short' }; // Сокращённое название месяца
 	let day = now.getDate();
@@ -70,10 +35,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	// Убираем точку и делаем первую букву заглавной
 	month = month.replace('.', '').charAt(0).toUpperCase() + month.slice(1, -1);
-
 	dateContainer.innerHTML = `<div class="day">${day}</div> <div class="data-wrapper">
 <div class="month">${month}</div></div>`;
-
 
 	//* --------------------------- Animation Header -----------------------------
 	const header = document.querySelector('.header');
@@ -105,7 +68,37 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+//* ----------------------------------------------------------------------------
+const projectList = document.querySelector('.project-list');
+const burgerButtons = document.querySelector('.burger-button');
+const listContent = document.querySelector('.main-content__project-list');
 
+burgerButtons.addEventListener('click', () => {
+	const backgroundColorTransparent = getComputedStyle(
+		document.documentElement).getPropertyValue(
+			'--background-color-transparent');
+	burgerButtons.firstChild.classList.toggle('is-active');
+	projectList.classList.toggle('_open-list');
+	document.body.classList.toggle('no-scroll');
+	if (projectList.classList.contains('_open-list')) {
+		// listContent.style.pointerEvents = 'all';
+		listContent.style.backgroundColor = backgroundColorTransparent;
+		document.body.classList.add('no-scroll');
+	} else {
+		document.body.classList.remove('no-scroll');
+		listContent.style.backgroundColor = 'transparent';
+		// listContent.style.pointerEvents = 'none';
+	}
+});
+
+// const closeButton = document.querySelector('.project-list__close-button');
+// closeButton.addEventListener('click', () => {
+// 	document.body.classList.remove('no-scroll');
+// 	burgerButtons.firstChild.classList.remove('is-active');
+// 	projectList.classList.remove('_open-list');
+// 	listContent.style.pointerEvents = 'none';
+// 	listContent.style.backgroundColor = 'transparent';
+// });
 //* ----------------------------------------------------------------------------
 console.log('%c РОССИЯ ',
 	'background: blue; color: yellow; font-size: x-large; ' +
