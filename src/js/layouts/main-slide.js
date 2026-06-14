d431a4ac43179397ef1ced6bfa78a1142d95f58d
Alexandr <104072970+Dennchik@users.noreply.github.com>
2026-06-14 13:28:41 +0300

    14.06.26.13.28

--- src/js/layouts/main-slide.js
+++ src/js/layouts/main-slide.js
@@ -1,170 +1,172 @@
-
 //* import Swiper bundle with all modules installed
 import Swiper from 'swiper/bundle';
 
-
 export function swiperPerform(Slide) {
-	const isMobile = /Android|iPhone|iPad|iPod|Windows Phone/i.test(navigator.userAgent);
-	if (Slide) {
-		const todayIndex = new Date().getDay(); // 0 — воскресенье, 1 — понедельник, ..., 6 — суббота
-		const cursor = document.querySelector('.slide-perform');
-		new Swiper(Slide, {
-			// speed: 800,
-			spaceBetween: 20,
-			effect: 'fade',
-			// grabCursor: true,
-			initialSlide: todayIndex, // <-- добавили эту строку
-			on: {
-				touchStart: () => cursor.classList.add('is-grabbing'),
-				touchEnd: () => cursor.classList.remove('is-grabbing'),
-			},
-			// loop: true,
-			// slidesPerView: 1,
-			// autoplay: {
-			// 	delay: 1500,
-			// 	disableOnInteraction: true,
-			// },
-			mousewheel: isMobile ? false : {
-				invert: false,
-			},
-			pagination: {
-				el: '.perform-slide__pagination',
-				clickable: true,
-			},
-			navigation: {
-				nextEl: '.slide-perform-next',
-				prevEl: '.slide-perform-prev ',
-			}
-		});
-	}
+  const isMobile = /Android|iPhone|iPad|iPod|Windows Phone/i.test(
+    navigator.userAgent
+  );
+  if (Slide) {
+    const todayIndex = new Date().getDay(); // 0 — воскресенье, 1 — понедельник, ..., 6 — суббота
+    const cursor = document.querySelector('.slide-perform');
+    new Swiper(Slide, {
+      // speed: 800,
+      spaceBetween: 20,
+      effect: 'fade',
+      // grabCursor: true,
+      initialSlide: todayIndex, // <-- добавили эту строку
+      on: {
+        touchStart: () => cursor.classList.add('is-grabbing'),
+        touchEnd: () => cursor.classList.remove('is-grabbing'),
+      },
+      // loop: true,
+      // slidesPerView: 1,
+      // autoplay: {
+      // 	delay: 1500,
+      // 	disableOnInteraction: true,
+      // },
+      mousewheel: isMobile
+        ? false
+        : {
+            invert: false,
+          },
+      pagination: {
+        el: '.perform-slide__pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.slide-perform-next',
+        prevEl: '.slide-perform-prev ',
+      },
+    });
+  }
 }
 
 export function newslide(Slide) {
-	if (Slide) {
-		new Swiper(Slide, {
-			speed: 800,
-			spaceBetween: 20,
-			loop: true,
-			grabCursor: true,
-			slidesPerView: 4,
-			centeredSlides: false,
-			navigation: {
-				nextEl: '.news-slide-next',
-				prevEl: '.news-slide-prev',
-			},
-			breakpoints: {
-				260: {
-					slidesPerView: 1,
-				},
-				540: {
-					slidesPerView: 2,
-				},
-				960: {
-					slidesPerView: 3,
-				},
-				1440: {
-					slidesPerView: 4,
-				}
-			}
-		});
-	}
+  if (Slide) {
+    new Swiper(Slide, {
+      speed: 800,
+      spaceBetween: 20,
+      loop: true,
+      grabCursor: true,
+      slidesPerView: 4,
+      centeredSlides: false,
+      navigation: {
+        nextEl: '.news-slide-next',
+        prevEl: '.news-slide-prev',
+      },
+      breakpoints: {
+        260: {
+          slidesPerView: 1,
+        },
+        540: {
+          slidesPerView: 2,
+        },
+        960: {
+          slidesPerView: 3,
+        },
+        1440: {
+          slidesPerView: 4,
+        },
+      },
+    });
+  }
 }
 
 export function templesSlide(Slide) {
-	if (Slide) {
-		new Swiper(Slide, {
-			speed: 800,
-			spaceBetween: 0,
-			loop: true,
-			grabCursor: true,
-			slidesPerView: 3,
-			centeredSlides: true,
-			navigation: {
-				nextEl: '.slide-temples__button-next',
-				prevEl: '.slide-temples__button-prev',
-			},
-			breakpoints: {
-				260: {
-					slidesPerView: 1,
-					spaceBetween: 10,
-				},
+  if (Slide) {
+    new Swiper(Slide, {
+      speed: 800,
+      spaceBetween: 0,
+      loop: true,
+      grabCursor: true,
+      slidesPerView: 3,
+      centeredSlides: true,
+      navigation: {
+        nextEl: '.slide-temples__button-next',
+        prevEl: '.slide-temples__button-prev',
+      },
+      breakpoints: {
+        260: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
 
-				1024: {
-					slidesPerView: 3,
-				}
-			}
-		});
-	}
+        1024: {
+          slidesPerView: 3,
+        },
+      },
+    });
+  }
 }
 
 export function websiteSlelide(Slide) {
-	if (Slide) {
-		new Swiper(Slide, {
-			speed: 800,
-			spaceBetween: 20,
-			// loop: true,
-			grabCursor: true,
-			slidesPerView: 5,
-			centeredSlides: false,
-			navigation: {
-				nextEl: '.slide-website-next',
-				prevEl: '.slide-website-prev',
-			},
-			breakpoints: {
-				260: {
-					slidesPerView: 1,
-					spaceBetween: 10,
-				},
-				490: {
-					slidesPerView: 2,
-				},
-				691: {
-					slidesPerView: 3,
-				},
-				1241: {
-					slidesPerView: 4,
-				},
-				1640: {
-					slidesPerView: 5,
-				}
-			}
-		});
-	}
+  if (Slide) {
+    new Swiper(Slide, {
+      speed: 800,
+      spaceBetween: 20,
+      // loop: true,
+      grabCursor: true,
+      slidesPerView: 5,
+      centeredSlides: false,
+      navigation: {
+        nextEl: '.slide-website-next',
+        prevEl: '.slide-website-prev',
+      },
+      breakpoints: {
+        260: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        490: {
+          slidesPerView: 2,
+        },
+        691: {
+          slidesPerView: 3,
+        },
+        1241: {
+          slidesPerView: 4,
+        },
+        1640: {
+          slidesPerView: 5,
+        },
+      },
+    });
+  }
 }
 
 export function slideSchedule(Slide) {
-	if (Slide) {
-		const todayIndex = (new Date().getDay()); // 0 — воскресенье, 1 — понедельник, ..., 6 — суббота
-		new Swiper(Slide, {
-			speed: 800,
-			loop: true,
-			spaceBetween: 20,
-			grabCursor: true,
-			initialSlide: todayIndex, // <-- добавили эту строку
+  if (Slide) {
+    const todayIndex = new Date().getDay(); // 0 — воскресенье, 1 — понедельник, ..., 6 — суббота
+    new Swiper(Slide, {
+      speed: 800,
+      // loop: true,
+      spaceBetween: 20,
+      grabCursor: true,
+      initialSlide: todayIndex, // <-- добавили эту строку
 
-			centeredSlides: false,
-			navigation: {
-				nextEl: '.slide-schedule-next',
-				prevEl: '.slide-schedule-prev',
-			},
-			breakpoints: {
-				260: {
-					slidesPerView: 1,
-					spaceBetween: 10,
-				},
-				490: {
-					slidesPerView: 2,
-				},
-				691: {
-					slidesPerView: 3,
-				},
-				1241: {
-					slidesPerView: 4,
-				},
-				1640: {
-					slidesPerView: 5,
-				},
-			}
-		});
-	}
+      centeredSlides: false,
+      navigation: {
+        nextEl: '.slide-schedule-next',
+        prevEl: '.slide-schedule-prev',
+      },
+      breakpoints: {
+        260: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        490: {
+          slidesPerView: 2,
+        },
+        691: {
+          slidesPerView: 3,
+        },
+        1241: {
+          slidesPerView: 4,
+        },
+        1640: {
+          slidesPerView: 5,
+        },
+      },
+    });
+  }
 }
